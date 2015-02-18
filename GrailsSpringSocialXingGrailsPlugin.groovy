44dfc95fa894c1d5e0c2637da11da38446963f53
Szymon Połom <szymon.polom@outfittery.de>
2015-02-18 17:40:24 +0100

    DEV-2768: upgraded dependencies

--- GrailsSpringSocialXingGrailsPlugin.groovy
+++ GrailsSpringSocialXingGrailsPlugin.groovy
@@ -1,8 +1,8 @@
 class GrailsSpringSocialXingGrailsPlugin {
     // the plugin version
-    def version = "0.1.34-SNAPSHOT"
+    def version = "0.2.0-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.3 > *"
+    def grailsVersion = "2.3.9 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
