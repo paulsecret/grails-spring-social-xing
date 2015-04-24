e9529ee4c6d3395dad6d33ccb6b64bbc7926b196
Szymon Połom <szymon.polom@outfittery.de>
2015-04-24 10:45:16 +0200

    new version

--- SpringSocialXingGrailsPlugin.groovy
+++ SpringSocialXingGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class SpringSocialXingGrailsPlugin {
     // the plugin version
-    def version = "0.2.0-SNAPSHOT"
+    def version = "0.2.0"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3.9 > *"
     // resources that are excluded from plugin packaging
